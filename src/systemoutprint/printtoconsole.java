f5f377cec4b714947a3a1301e9184565220ab081
martinalex84 <53490065+martinalex84@users.noreply.github.com>
2019-07-30 18:56:28 +0100

    Add files via upload

--- src/systemoutprint/printtoconsole.java
+++ src/systemoutprint/printtoconsole.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Free.
  */
 package systemoutprint;
 
@@ -9,7 +7,7 @@ import java.util.List;
 
 /**
  *
- * @author columbo
+ * @author Alex Martin
  */
 public class printtoconsole 
 {
